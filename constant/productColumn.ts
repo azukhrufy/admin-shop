c9b9728525f2cc5ee5f6a696a20bba113d596b49
azukhrufy <anandazukhrufawalwi@gmail.com>
2023-02-16 16:05:54 +0700

    :hammer: FE: refactor style of table and toolbar

--- constant/productColumn.ts
+++ constant/productColumn.ts
@@ -2,39 +2,43 @@ import { GridColDef } from "@mui/x-data-grid";
 
 export const columns: GridColDef[] = [
     { field: "id", headerName: "ID", hide: true },
-    { field: "productName", headerName: "Product Name", flex: 2 },
-    { field: "brand", headerName: "Brand", flex: 1 },
+    { field: "productName", headerName: "Product Name", flex: 2, minWidth: 100 },
+    { field: "brand", headerName: "Brand", flex: 1, minWidth: 100 },
     {
       field: "price",
       headerName: "Price",
       type: "number",
       flex: 1,
+      minWidth: 100
     },
     {
       field: "stock",
       headerName: "Stock",
       type: "number",
       flex: 1,
+      minWidth: 100
     },
-    { field: "category", headerName: "Category", flex: 1 },
+    { field: "category", headerName: "Category", flex: 1, minWidth: 100 },
   ];
 
   export const prodColumns: GridColDef[] = [
     { field: "id", headerName: "ID", hide: true },
-    { field: "productName", headerName: "Product Name", flex: 2 },
-    { field: "quantity", headerName: "Quantity", flex: 1 },
-    { field: "discount", headerName: "Discount", flex: 1 },
+    { field: "productName", headerName: "Product Name", flex: 2, minWidth: 100 },
+    { field: "quantity", headerName: "Quantity", flex: 1, minWidth: 100 },
+    { field: "discount", headerName: "Discount", flex: 1, minWidth: 100 },
     {
       field: "price",
       headerName: "Price",
       type: "number",
       flex: 1,
+      minWidth: 100
     },
     {
       field: "total",
       headerName: "Total",
       type: "number",
       flex: 1,
+      minWidth: 100
     },
-    { field: "discountedPrice", headerName: "Discounted Price", flex: 1 },
+    { field: "discountedPrice", headerName: "Discounted Price", flex: 1, minWidth: 100 },
   ];
